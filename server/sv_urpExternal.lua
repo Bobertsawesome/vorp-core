eb59814f75d1a26b15720cf55a6eb2ece433b422
Bobertsawesome <43273928+Bobertsawesome@users.noreply.github.com>
2022-06-17 03:10:41 -0500

    Wait on resource start
    Gives extra time to make sure export is ready

--- server/sv_urpExternal.lua
+++ server/sv_urpExternal.lua
@@ -1,5 +1,6 @@
 AddEventHandler('onResourceStart', function(resourceName)
   if resourceName == 'urpUtil' then
+	Wait(1000)
 	local data = exports.urpUtil:webhookExport('vorp_core')
 	if data then
 		for k,v in pairs(data) do
