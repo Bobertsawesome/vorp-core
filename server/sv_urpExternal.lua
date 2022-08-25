83a42ea60f95efb71a2cd6ace7fc885cd3750454
Bobertsawesome <43273928+Bobertsawesome@users.noreply.github.com>
2022-08-25 00:55:24 -0500

    Webhook import module

--- server/sv_urpExternal.lua
+++ server/sv_urpExternal.lua
@@ -1,11 +1,26 @@
-AddEventHandler('onResourceStart', function(resourceName)
-  if resourceName == 'urpUtil' then
-	Wait(1000)
-	local data = exports.urpUtil:webhookExport('vorp_core')
-	if data then
-		for k,v in pairs(data) do
-			Config[k] = v
-		end
+Citizen.CreateThread(function()
+	local counter = 1
+	while GetResourceState('urpUtil') ~= "started" and counter < 50 do
+		counter = counter + 1
+		Wait(500)
 	end
-  end
+	if GetResourceState('urpUtil') == "started" then
+		Wait(500)
+		local data = exports.urpUtil:webhookExport('vorp_core')
+		if data then
+			data = msgpack.unpack(data)
+			for k,v in pairs(data) do
+				if Config and Config[k] then
+					Config[k] = v
+				else
+					print("^1Webhook Import Error! ^3Table[^7" .. k .. "^3] no longer exists or has changed!^7")
+					return
+				end
+			end
+		else
+			print("^1Webhook Import Error! ^3Requested table no longer exists or is nil!^7")
+		end
+	else
+		print("^1Webhook Import Error! ^3GetResourceState timeout! ^7" .. GetResourceState('urpUtil'))
+	end
 end)
